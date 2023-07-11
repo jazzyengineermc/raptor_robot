a2c69a3c9edadd26dacdb8347dfbc46b44586039
jazzyengineermc <jazzyengineermc@gmail.com>
2023-07-10 22:51:04 -0400

    working on halt/stop function

--- src/homebin/cancel_goal.sh
+++ src/homebin/cancel_goal.sh
@@ -2,4 +2,6 @@
 cd /home/jreide/catkin_ws
 source devel/setup.bash
 
-rostopic pub -l /move_base/cancel actionlib_msgs/GoalID -- {}
+rostopic pub -l /move_base/cancel actionlib_msgs/GoalID -- {} &
+
+echo "done"
