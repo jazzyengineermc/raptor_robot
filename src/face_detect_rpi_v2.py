62be197a60b9b2ce4fdeee97b39eff632a1e9920
jazzyengineermc <jazzyengineermc@gmail.com>
2023-07-11 22:31:55 -0400

    doh

--- src/face_detect_rpi_v2.py
+++ src/face_detect_rpi_v2.py
@@ -54,9 +54,9 @@ def face_detect():
     face_cascade = cv2.CascadeClassifier(
         "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
     )
-    eye_cascade = cv2.CascadeClassifier(
-        "/usr/share/opencv4/haarcascades/haarcascade_eye.xml"
-    )
+    #eye_cascade = cv2.CascadeClassifier(
+    #   "/usr/share/opencv4/haarcascades/haarcascade_eye.xml"
+    #)
     video_capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
         try:
@@ -66,15 +66,27 @@ def face_detect():
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+                roi = ''
+                max_area = 0
+                area = 0
                 for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    roi_gray = gray[y : y + h, x : x + w]
-                    roi_color = frame[y : y + h, x : x + w]
-                    eyes = eye_cascade.detectMultiScale(roi_gray)
-                    for (ex, ey, ew, eh) in eyes:
-                        cv2.rectangle(
-                            roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2
-                        )
+                    cv2.rectangle(frame, (x, y), (x+w,y+h), (255, 0, 255), 2)  #  I put a box around your head ;P
+                    #  now find the biggest face and report that one
+                    area=h*w
+                    if area>max_area:
+                       max_area=area
+                       # roi=img[y:y+h, x:x+w]
+                       roi = [x, y, w, h]
+                       
+                #for (x, y, w, h) in faces:
+                #    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                #    roi_gray = gray[y : y + h, x : x + w]
+                #    roi_color = frame[y : y + h, x : x + w]
+                    #eyes = eye_cascade.detectMultiScale(roi_gray)
+                    #for (ex, ey, ew, eh) in eyes:
+                    #    cv2.rectangle(
+                    #       roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2
+                    #   )
                 # Check to see if the user closed the window
                 # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
                 # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
